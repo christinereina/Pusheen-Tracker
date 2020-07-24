b25b46558e1a6bbde8aed045ab98b3bf70db791a
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-24 16:44:02 -0700

    pass 1st test for Order

--- Pusheen.Tests/ModelTests/OrderTest.cs
+++ Pusheen.Tests/ModelTests/OrderTest.cs
@@ -5,5 +5,16 @@ using System.Collections.Generic;
 
 namespace PusheenTracker.Tests
 {
-  
+
+ [TestClass]
+  public class OrderTest
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("5 red bean buns");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+
 }
--- Pusheen/Models/Order.cs
+++ Pusheen/Models/Order.cs
@@ -4,7 +4,34 @@ namespace PusheenTracker.Models
 {
   public class Order
   {
+    public string OrderType { get; set; }
 
-    
+    public string OrderQuantity { get; set; }
+
+    public int OrderPrice { get; set; }
+
+    public int Id { get; }
+
+    private static List<Order> _orderDetails = new List<Order>{};
+
+    public Order(string orderType)
+    {
+      OrderType = orderType;
+      _orderDetails.Add(this);
+      Id = _orderDetails.Count;
+    }
+
+    public Order(string orderType, string orderQuantity)
+      : this(orderType)
+    {
+      OrderQuantity = orderQuantity;
+    }
+
+     public Order(string orderType, string orderQuantity, int orderPrice)
+      : this(orderType, orderQuantity)
+    {
+     OrderPrice = orderPrice;
+    }
+ 
   }
 }
