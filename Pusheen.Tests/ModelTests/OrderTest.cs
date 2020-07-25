2bba2e684c265fa0219671ee2aa81100112b0fb3
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-24 17:59:57 -0700

    fail Order tests to return Order properties

--- Pusheen.Tests/ModelTests/OrderTest.cs
+++ Pusheen.Tests/ModelTests/OrderTest.cs
@@ -16,6 +16,13 @@ namespace PusheenTracker.Tests
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
+    [TestMethod]
+      public void OrderConstructor_CreatesInstanceOfOrder_Quantity()
+     {
+      Order newOrder = new Order("Red Bean Buns", 20 );
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+     }
+
      [TestMethod]
       public void OrderConstructor_CreatesInstanceOfOrder_Price()
      {
@@ -32,6 +39,19 @@ namespace PusheenTracker.Tests
       Assert.AreEqual(orderType, result);
     }
 
+    [TestMethod]
+      public void GetProperties_ReturnOrderProperties_String()
+    {
+      string orderType = "Green Tea Cloud Cakes";
+      int orderQuantity = 20;
+      int orderPrice = 200;
+      Order newOrder = new Order(orderType, orderQuantity, orderPrice);
+      string orderTypeResult = newOrder.OrderType;
+      int orderQuantityResult = newOrder.OrderQuantity;
+      int orderPriceResult = newOrder.OrderPrice;
+      Assert.AreEqual(orderType, orderTypeResult);
+      Assert.AreEqual(orderQuantity, orderQuantityResult);
+      Assert.AreEqual(orderPrice, orderPriceResult);      
+    }
   }
-
 }
